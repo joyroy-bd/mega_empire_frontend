a2219dc75fb689e110f5d8d7d098aca2098c7214
joyroy-bd <joyroy261216@gmail.com>
2022-03-31 01:00:14 +0600

    All Done

--- src/components/warlogInfo/WarDetails.jsx
+++ src/components/warlogInfo/WarDetails.jsx
@@ -3,7 +3,7 @@ import useWarDetails from "../../hooks/useWarDetails";
 
 export default function WarDetails({ opponentTag }) {
   console.log(opponentTag)
-  let res = useWarDetails("#28CG829QR");
+  let res = useWarDetails(opponentTag);
   // /:tag/wardetails/:endtime
   console.log(res);
   return <>{ <p className="container">{opponentTag} - Found!</p>}</>;
